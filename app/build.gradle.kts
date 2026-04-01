028da40faa95336a5056efdf68e0dbe07893f03c
celestialtaha <tahasamavati11@yahoo.com>
2026-04-01 22:09:37 +0200

    chore(release): bump app version to 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.goodwy.smsmessenger"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
     }
 
